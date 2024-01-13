e6fe096a63c09463d53dd4cc9362746c77b0393c
Misha Prochan <konnko@Mishas-MacBook-Pro.local>
2024-01-14 00:53:04 +0300

    add elixir and heex to treesitter, swap j k for better convinience on colemak

--- mappings.lua
+++ mappings.lua
@@ -11,17 +11,29 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    ["<leader>fp"] = { "<cmd>Telescope projects<CR>", desc = "Search projects" }
+    ["<leader>fp"] = { "<cmd>Telescope projects<CR>", desc = "Search projects" },
 
+    ["j"] = { "k" },
+    ["k"] = { "j" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  },
+  o = {
+    ["j"] = { "k" },
+    ["k"] = { "j" },
+  },
+  x = {
+    ["j"] = { "k" },
+    ["k"] = { "j" },
   },
   t = {
     -- setting a mapping to false will disable it
